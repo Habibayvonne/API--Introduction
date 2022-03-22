b3a298eb2e931f29371848765ac7c5add545deae
rapando <samsonrapando@gmail.com>
2022-03-22 20:59:12 +0300

    lesson 3

--- api-tutorial.go
+++ api-tutorial.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/innv8/api-introduction/controllers"
-)
+import "github.com/innv8/api-introduction/controllers"
 
 /*
 To use 'air' in any project, first run this to make your project a go module
@@ -18,14 +13,30 @@ go mod tidy
 */
 
 func main() {
-	port := ":5000"
-	log.Printf("starting API on port %s", port)
 
-	http.HandleFunc("/", controllers.HomeController)
-	http.HandleFunc("/users", controllers.UsersController)
+	/*
+		port := ":5000"
+		log.Printf("starting API on port %s", port)
 
-	err := http.ListenAndServe(port, nil)
-	if err != nil {
-		log.Printf("unable to start API because %s", err)
-	}
+		// we create a router variable and add the routes the same way we did with http
+		// then we put router as the second parameter in http.ListenAndServe method
+		var router = mux.NewRouter()
+
+
+		// http.HandleFunc("/", controllers.HomeController)
+		// http.HandleFunc("/users", controllers.UsersController)
+
+		err := http.ListenAndServe(port, router)
+		if err != nil {
+			log.Printf("unable to start API because %s", err)
+		}
+	*/
+
+	// we are now using a controllers.Base struct to hold the router, this means
+	// we also need to start the API from controllers.
+	// because if you import a struct from another package, you cannot modify its pointer
+
+	var base controllers.Base
+	base.StartRouter()
+	base.StartAPI()
 }
